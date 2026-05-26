9aaad519ed1d8726cb9d24ecd97a32bad473fedf
Mitesh Paliwal <113752545+miteshp2110@users.noreply.github.com>
2026-05-26 14:07:33 +0530

    Update API URL in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,8 @@
 // export const environment = {
 //   production: false,
-//   apiUrl: 'https://riviobackendspringboot-production.up.railway.app/api'
-// };
-
-// export const environment = {
-//   production: false,
 //   apiUrl: 'http://localhost:8081/api'
 // };
 export const environment = {
   production: false,
-  apiUrl: 'https://riviobackendspringboot-production-6514.up.railway.app/api'
+  apiUrl: 'https://riviobackendspringboot-production-a9df.up.railway.app/api'
 };
